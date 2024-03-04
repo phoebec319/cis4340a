406f3101c9d20990c45630add7f25f97ce0202dc
Phoebe319 <phoebe@thephoebeshow.com>
2024-03-03 21:41:23 -0500

    edits

---
+++ R04_STR03_J.java
@@ -0,0 +1,7 @@
+public class R04_STR03_J {
+    BigInteger x = new BigInteger(”530500452766”);
+    String s = x.toString(); // Valid character data
+    byte[] byteArray = s.getBytes();
+    String ns = new String(byteArray);
+    x = new BigInteger(ns);
+}
